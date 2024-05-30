ded4dba8761f4cf08f42f02aea9da9c1e0019962
KOUASSI Désiré <desire.kouassi@mobisoft.ci>
2024-05-30 09:54:14 +0000

    feat: Mise en place du model(bd) de templte

--- server/database/schema.ts
+++ server/database/schema.ts
@@ -1,3 +1,4 @@
+import { relations } from 'drizzle-orm'
 import { integer, sqliteTable, text } from 'drizzle-orm/sqlite-core'
 
 export const users = sqliteTable('users', {
@@ -29,3 +30,32 @@ export const modules = sqliteTable('modules', {
   createdAt: text('created_at').notNull().$defaultFn(() => sql`(current_timestamp)`),
   updatedAt: text('updated_at').notNull().$defaultFn(() => sql`(current_timestamp)`).$onUpdateFn(() => sql`(current_timestamp)`),
 })
+
+export const templates = sqliteTable('templates', {
+  id: integer('id').primaryKey({ autoIncrement: true }),
+  hash: text('name').notNull().unique(),
+  slug: text('slug').notNull(),
+  title: text('title').notNull(),
+  /**
+   * Submitted: when a user submits a template
+   * Refused: when a user's template is refused by an admin
+   * Validate: when a user's template is validated by an admin and visible by everyone
+   */
+  status: text('status', { enum: ['submitted', 'refused', 'validate'] }).notNull(),
+  paidStatus: text('paid_status', { enum: ['free', 'paid'] }).notNull().default('free'),
+  liveUrl: text('live_url'),
+  accessUrl: text('access_url').notNull(),
+  description: text('description').notNull(),
+  userId: integer('user_id').references(() => users.id),
+  categoryId: integer('category_id').references(() => categories.id),
+  createdAt: text('created_at').notNull().$defaultFn(() => sql`(current_timestamp)`),
+  updatedAt: text('updated_at').notNull().$defaultFn(() => sql`(current_timestamp)`).$onUpdateFn(() => sql`(current_timestamp)`),
+})
+
+export const usersRelation = relations(users, ({ many }) => ({
+  templates: many(templates),
+}))
+
+export const categoryRelation = relations(categories, ({ many }) => ({
+  templates: many(modules),
+}))
