2f3ca136b87e39be07734cbe055f1971156a29ca
KOUASSI Désiré <desire.kouassi@mobisoft.ci>
2024-06-04 17:49:41 +0000

    chore: replace customNanoid with nanoid

--- server/utils/hash.ts
+++ server/utils/hash.ts
@@ -1,9 +1,5 @@
-import { customAlphabet, urlAlphabet } from 'nanoid'
+import { nanoid } from 'nanoid'
 
-/***
- * We replace the "-" character with an empty string to easily split the URL.
- */
-const nanoid = customAlphabet(urlAlphabet.replace('-', ''))
 export function useHash() {
   return nanoid(12)
 }
