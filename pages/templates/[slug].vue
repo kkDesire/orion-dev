15ed6bc202372b7501d8c1e5195fe46aa76b2212
KOUASSI Désiré <desire.kouassi@mobisoft.ci>
2024-06-04 18:17:10 +0000

    feat: Visualazation a template with his manage url

---
+++ pages/templates/[slug].vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+const route = useRoute()
+
+const hash = computed(() => route.params.slug.slice(-12))
+
+const { data: template } = await useFetch(`/api/templates/${hash.value}`, {
+  deep: false,
+  default: () => {},
+})
+</script>
+
+<template>
+  <div>{{ template }}</div>
+</template>
+
+<style scoped>
+
+</style>
