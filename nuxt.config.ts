e0b582a91ea6444d0dd252135dda28b6ded9b3e8
KOUASSI Désiré <desire.kouassi@mobisoft.ci>
2024-06-05 15:57:52 +0000

    chore: add nuxt eslint module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,31 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      htmlAttrs: {
+        dir: 'ltr',
+        class: 'scroll-smooth',
+      },
+    },
+  },
+
   devtools: { enabled: true },
   extends: ['@nuxt/ui-pro'],
-  modules: ['nuxt-auth-utils', '@nuxthub/core', '@nuxt/ui'],
+  modules: ['nuxt-auth-utils', '@nuxthub/core', '@nuxt/ui', '@nuxt/eslint'],
   runtimeConfig: {
     oauth: {
       github: {
         clientId: '',
         clientSecret: '',
       },
+    },
+  },
+  ui: {
+    icons: ['heroicons', 'simple-icons'],
+  },
+  eslint: {
+    config: {
+      stylistic: true,
     },
   },
   hub: {
